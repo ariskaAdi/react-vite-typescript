c3af443eab99c280f909a2e01ee855df9331cdeb
ariskaAdi <ariskaadiprasetyo@gmail.com>
2024-05-23 09:15:23 +0700

    data fetching in react

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,29 @@
-import "./App.css";
-import Exam from "./components/effect";
-function App() {
-  // const peopleName = {
-  //   firstName: "John",
-  //   lastName: "Doe",
-  // };
-  return (
-    <>
-      {/* <Person name="jhon" age={20} isLoggedIn={true}></Person>
-      <People name={peopleName}></People> */}
-      <Exam />
-    </>
-  );
+import { useEffect, useState } from "react";
+const BASE_URL = "https://jsonplaceholder.typicode.com";
+interface Post {
+  id: number;
+  title: string;
 }
 
-export default App;
+export default function App() {
+  const [posts, setPosts] = useState<Post[]>([]);
+
+  useEffect(() => {
+    const fetchPost = async () => {
+      const response = await fetch(`${BASE_URL}/posts`);
+      const posts = (await response.json()) as Post[];
+      setPosts(posts);
+    };
+    fetchPost();
+  }, []);
+  return (
+    <div>
+      <h1>Data fetching in react</h1>
+      <ul>
+        {posts.map((post) => {
+          return <li key={post.id}>{post.title}</li>;
+        })}
+      </ul>
+    </div>
+  );
+}
